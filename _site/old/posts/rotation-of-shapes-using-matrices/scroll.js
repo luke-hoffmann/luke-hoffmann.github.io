ebe52aa8a8d19571275fb27b697bd043aba9a7a3
luke-hoffmann <195851152+luke-hoffmann@users.noreply.github.com>
2025-01-27 22:07:45 -0800

    finished porting everything to jekyll

--- _site/old/posts/rotation-of-shapes-using-matrices/scroll.js
+++
@@ -1,8 +0,0 @@
-window.onbeforeunload = function () {
-    window.scrollTo(0, 0);
-}
-function scrollToPoint() {
-    // Scroll to the element with the ID "target"
-    const targetElement = document.getElementsByClassName("target")[0];
-    targetElement.scrollIntoView({ behavior: 'smooth' });
-}
