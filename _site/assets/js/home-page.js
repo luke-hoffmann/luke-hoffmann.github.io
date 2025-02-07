60f16b8593f1b4f4a4fdc0a2feb6b90bd2b50879
luke-hoffmann <39360743+luke-hoffmann@users.noreply.github.com>
2025-02-06 22:52:52 -0800

    .gitignore is now working

--- _site/assets/js/home-page.js
+++
@@ -1,8 +0,0 @@
-window.onbeforeunload = function () {
-    window.scrollTo(0, 0);
-}
-function scrollToPoint() {
-    // Scroll to the element with the ID "target"
-    const targetElement = document.getElementById("target");
-    targetElement.scrollIntoView({ behavior: 'smooth' });
-}
--- open-terminal.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "%~dp0"
-call jekyll clean
-start "" jekyll serve
-timeout /t 5 /nobreak > nul
-start "" "http://127.0.0.1:4000"
-exit
