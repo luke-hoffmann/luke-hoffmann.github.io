3a1f38882571e72a7fb025162df22ac63da54c1a
luke-hoffmann <39360743+luke-hoffmann@users.noreply.github.com>
2025-02-06 22:23:49 -0800

    changed the summary for 3d convex hull

--- open-terminal.bat
+++ open-terminal.bat
@@ -1,6 +1,7 @@
 @echo off
 cd /d "%~dp0"
-rundll32 url.dll,FileProtocolHandler http://127.0.0.1:4000
 call jekyll clean
-call jekyll serve
-pause
+start "" jekyll serve
+timeout /t 5 /nobreak > nul
+start "" "http://127.0.0.1:4000"
+exit
